2274ecb67701d45bdb478aba3da0264dd013ce0f
wws <351483565@qq.com>
2021-03-20 18:05:45 -0700

    updates

--- Publisher.py
+++ Publisher.py
@@ -17,24 +17,24 @@ from datetime import datetime
 import time
 from scipy import stats
 from collections import defaultdict
+import requests
 
 channel = "heart-rate"
-patient_id = "2"
-patient_name = "Zhenghao"
-location = "Room A"
+# patient_id = "2"
+# patient_name = "Zhenghao"
+# location = "Room A"
 # host_name = "192.168.50.121"
 host_name = "127.0.0.1"
+
+
 
 #publisher = redis.Redis(host = host_name, port = 6379)
 publisher = redis.Redis.from_url('redis://3.tcp.ngrok.io:23327')
 
 date = str(datetime.now())
+URL_host_list = ["http://192.168.0.148"] # retrieve bph data
+raw_data_path = '/raw-data'
 
-#  RECIEVE DATA FROM EDGE
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1) #esp bpm
-ser2 = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)#location
-ser.flush()
-ser2.flush()
 
 start_sending_location = False
 
@@ -45,60 +45,72 @@ patient_record_temp = defaultdict(list)
 
 start_time = time.time()
 
+{
+    "Patient_id":"01", 
+    "Device_id":"ESP32-USR03",
+    "Patient_first_name":"Lu",
+    "Patient_last_name":"Lu",
+    "BPM":"0",
+    "SPO2":"-999","TEMP":"0.00",
+    "AP":{"AP1":-90,"AP2":-90,"AP3":-90,"AP4":-90,"AP5":-90,"AP6":-90,"AP7":-90,"AP8":-90,"AP9":-90,"AP10":-90},
+    "USR":{"USR0":-90,"USR1":-90,"USR2":-90,"USR3":-90}
+}
 while True:
     try:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            info = line.rstrip().split("|")
-            if(len(info) == 4):
+        for device_URL in URL_host_list:
+            r = requests.get(device_URL+raw_data_path)
+            data = eval(r.text)
+            Patient_id = data['Patient_id']
+            Device_id = data['Device_id']
+            patient_name = data['Patient_first_name']+" "+data['Patient_last_name']
+            bpm = data['BPM']
+
+            xPosition, yPosition = get_current_position(AP)
+            sdAlarm0,sdAlarm1,sdAlarm2 = get_SD_Alarms(USR)
+            infos = [Patient_id, Device_id, patient_name,xPosition,yPosition,sdAlarm0,sdAlarm1,sdAlarm2]
+            publisher.publish(channel, "|".join(infos))
+
+            location = (xPosition,yPosition)
+            location_data = Device_id+"|"+str(location) #+"|"+str(time.time())
+            publisher.publish(channel, location_data)
+            print(location_data)
+
+            if Patient_id not in patient_files:
+                file = open(date+"_record_"+ Patient_id +".txt", "w")
+                patient_files[Patient_id] = file
+
+            if Patient_id in db and len(patient_record_temp[Patient_id])>30:
+                # start recording after receiving 30 data; 
+                line = '|'.join([Patient_id, Device_id, patient_name, bpm])
+                db[Patient_id].append(line)
+            else:
+                db[Patient_id] = [line]
+                patient_record_temp[Patient_id].append(line)
                 
-                sent_data = line
-                publisher.publish(channel, sent_data)
-                start_sending_location = True
-                if info[0] in db and len(patient_record_temp[info[0]])>30:
-                    # start recording after receiving 30 data; 
-                    db[info[0]].append(line)
+            if Patient_id in db and len(db[Patient_id])!=0 and len(db[Patient_id]) % 30==0:
+                ml_result = deep_learning_api.ml_prediction( db[Patient_id] )#patient_file[Patient_id])
+                print("ml result is:      ",ml_result)
+                if ml_result is -1:
+                    pass
                 else:
-                    db[info[0]] = [line]
-                    patient_record_temp[info[0]].append(line)
-                    
-                    
-                if info[0] not in patient_files:
-                    file = open(date+"_record_"+ info[0] +".txt", "w")
-                    patient_files[info[0]] = file
-                    
-                    
-                if len(db[info[0]]) % 30==0:
-                    ml_result = deep_learning_api.ml_prediction( db[info[0]] )#patient_file[info[0]])
-                    print("ml result is:      ",ml_result)
-                    if ml_result is -1:
-                        pass
-                    else:
-                        
-                        publisher.publish(channel, info[1]+"|"+ deep_learning_api.convert(ml_result) +"|"+"00")
-                        patient_files[info[0]].write("\n".join(db[info[0]]))
-                        db[info[0]] = list()
-                        print("Result;  ", deep_learning_api.convert(ml_result))
-            print(line)
+                    publisher.publish(channel, Device_id+"|"+ deep_learning_api.convert(ml_result) +"|"+"00")
+                    patient_files[Patient_id].write("\n".join(db[Patient_id]))
+                    db[Patient_id] = list()
+                    print("Result;  ", deep_learning_api.convert(ml_result))
             
-        if ser2.in_waiting > 0:
-            line2 = ser2.readline().decode('utf-8').rstrip()
-            
-            info2 = line2.split("|")
-            if (len(info2) == 2 and start_sending_location):
-                
-                if time.time()-start_time>=5:
-                    start_time = time.time()
-                    location_data = str(int(info2[0]))+"|"+info2[1].replace(" ", "") #+"|"+str(time.time())
-                    publisher.publish(channel, location_data)
-                    print(location_data)
+        time.sleep(5)
                         
 
     except UnicodeDecodeError:
         print("decode exception once")
 
-# In[ ]:
 
+
+
+
+
+
+        
 
 
 
